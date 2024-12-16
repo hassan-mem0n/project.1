ea0010ee226dade5c6714f7aff731dfc4fb4b10e
hassan.mem0n <hassan.mem0n9897@gmail.com>
2024-12-17 01:42:26 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+let toDoList = [];
+
+
+let num1 = prompt("type your 1 number");
+let num2 = prompt("type your 2 number");
+let num3 = prompt("type your 3 number");
+
+
+
+toDoList.push(num1,num2,num3);
+
+
+console.log(`your task ${num1}`);
+console.log(`your task ${num2}`);
+console.log(`your task ${num3}`);
+
+
+
+
+console.log("to do list answer");
+console.log(`1. ${toDoList[0]}`);
+console.log(`2. ${toDoList[1]}`);
+console.log(`3. ${toDoList[2]}`);
